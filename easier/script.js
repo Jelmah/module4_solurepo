110200e9e617017c3f027908b339214048407deb
jelolo <jelolo@gmail.com>
2020-10-18 13:34:27 +0200

    commit two

--- easier/script.js
+++ easier/script.js
@@ -43,12 +43,12 @@ var names = ["Yaakov", "Mercy", "John", "Jerry", "Jelili", "Paul", "Jetli", "Lar
 // 'speak' method.
 // See Lecture 50, part 1
 for (var i = 0; i < names.length; i++) {
-
+                   
   // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
   // names that start with either upper case or lower case 'J'/'j', call
-  // string object's 'toLowerCase' method on the result so we can compare
+  // string object's 'toLowerCase' method on the result so we can compare     nb
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
   var firstLetter = names[i].charAt(0).toLowerCase();
